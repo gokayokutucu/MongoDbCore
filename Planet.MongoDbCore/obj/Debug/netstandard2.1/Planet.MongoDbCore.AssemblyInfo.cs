846f68ff1aa3dd70875461cb9de4cf525d479269
Gokay Okutucu <gokay@blinkdot.co>
2020-03-05 16:04:31 +0300

    Update readme and add .gitignore

--- Planet.MongoDbCore/obj/Debug/netstandard2.1/Planet.MongoDbCore.AssemblyInfo.cs
+++ Planet.MongoDbCore/obj/Debug/netstandard2.1/Planet.MongoDbCore.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Planet.MongoDbCore")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Blink")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
--- Readme.md
+++ Readme.md
@@ -1,1 +1,15 @@
 New MongoDbCore technology from Flash Gordon's adventure planet.
+
+### Nuget
+
+| | |
+|-|-|
+| downloads | ![](https://img.shields.io/nuget/dt/Planet.MongoDbCore) |
+| stable | ![](https://img.shields.io/nuget/v/Planet.MongoDbCore) |
+
+## Install
+
+    dotnet add package Planet.MongoDbCore
+
+
+License: Apache License 2.0
