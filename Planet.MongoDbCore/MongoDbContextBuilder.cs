6ebc59aa18d2648301b3a2f3bb31c1c78ba80131
Gokay Okutucu <gokay@blinkdot.co>
2020-04-06 12:04:15 +0300

    Update build method in builder]

--- Planet.MongoDbCore/MongoDbContextBuilder.cs
+++ Planet.MongoDbCore/MongoDbContextBuilder.cs
@@ -37,7 +37,7 @@ namespace Planet.MongoDbCore {
         public string Url { get; set; }
 
         public TService Build () {
-            return Build (null);
+            return Build (default (IServiceProvider));
         }
 
         public TService Build (IServiceProvider provider) {
@@ -56,7 +56,7 @@ namespace Planet.MongoDbCore {
             foreach (var p in constructorParamsInfo) {
                 if (p.ParameterType == typeof (MongoDbContextOptions))
                     constratorParams[i] = dbContextOptions;
-                else if (provider != null)
+                else if (provider != default (IServiceProvider))
                     constratorParams[i] = provider.GetService (p.ParameterType);
                 else
                     throw new ArgumentNullException ("You should pass a provider or database context options");
